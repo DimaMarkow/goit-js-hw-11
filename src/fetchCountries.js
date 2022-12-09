52e3e795236f0ed4794bb13cd9cf9b9ba0d81acb
DimaMarkow <dimamarkow@gmail.com>
2022-12-09 17:10:17 +0200

    start

---
+++ src/fetchCountries.js
@@ -0,0 +1,17 @@
+import Notiflix from 'notiflix';
+
+const refs = {
+  list: document.querySelector(`.country-list`),
+};
+
+export default function fetchCountries(name) {
+  return fetch(name).then(response => {
+    if (response.ok) {
+      return response.json();
+    } else {
+      // refs.list.innerHTML = ``;
+      Notiflix.Notify.failure('Oops, there is no country with that name');
+      refs.list.innerHTML = ``;
+    }
+  });
+}
